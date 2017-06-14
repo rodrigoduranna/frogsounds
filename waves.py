261525b02a0e0c604b9fb06b5e73f174eb7fae47
Rodrigo Silva Duran <rodrigo.duran@ifms.edu.br>
2017-06-14 14:22:16 +0300

    add comments and time elapsed in the NN training

--- waves.py
+++ waves.py
@@ -2,9 +2,12 @@ import glob
 import os
 import librosa
 import numpy as np
+import time
 import matplotlib.pyplot as plt
 import librosa.display
 from matplotlib.pyplot import specgram
+
+comeco = time.time()
 
 plt.style.use('ggplot')
 #Tamanho dos plots . Caso as imagens estiverem muito grandes ou pequenas, alterar aqui
@@ -65,41 +68,45 @@ def plot_log_power_specgram(sound_names,raw_sounds):
 sound_file_paths = ["sapo1.wav"]
 sound_names = ["Sapo-folha (Rhinella scitula) - Track 1"]
 #carrega os arquivos de som
-raw_sounds = load_sound_files(sound_file_paths)
+#raw_sounds = load_sound_files(sound_file_paths)
 #plota os graficos
-plot_waves(sound_names,raw_sounds)
-plot_specgram(sound_names,raw_sounds)
-plot_log_power_specgram(sound_names,raw_sounds)
 
-'''
+#plot_waves(sound_names,raw_sounds)
+#plot_specgram(sound_names,raw_sounds)
+#plot_log_power_specgram(sound_names,raw_sounds)
 
-print("Extracting features ...")
 
+
+print("Extraindo caracteristicas ...")
+
+#extrai as caracteristicas de um arquivo de som
 def extract_feature(file_name):
-    X, sample_rate = librosa.load(file_name)
-    stft = np.abs(librosa.stft(X))
-    mfccs = np.mean(librosa.feature.mfcc(y=X, sr=sample_rate, n_mfcc=40).T,axis=0)
-    chroma = np.mean(librosa.feature.chroma_stft(S=stft, sr=sample_rate).T,axis=0)
-    mel = np.mean(librosa.feature.melspectrogram(X, sr=sample_rate).T,axis=0)
-    contrast = np.mean(librosa.feature.spectral_contrast(S=stft, sr=sample_rate).T,axis=0)
-    tonnetz = np.mean(librosa.feature.tonnetz(y=librosa.effects.harmonic(X), sr=sample_rate).T,axis=0)
+    X, sample_rate = librosa.load(file_name) #extrai o numero de amostras do arquivo
+    stft = np.abs(librosa.stft(X)) #Short time Fourier transform do arquivo de som
+    mfccs = np.mean(librosa.feature.mfcc(y=X, sr=sample_rate, n_mfcc=40).T,axis=0) #MFCSS do arquivo de som
+    chroma = np.mean(librosa.feature.chroma_stft(S=stft, sr=sample_rate).T,axis=0) #chromatograma do arquivo da STFT
+    mel = np.mean(librosa.feature.melspectrogram(X, sr=sample_rate).T,axis=0) #Mel espectograma do araquivo de som
+    contrast = np.mean(librosa.feature.spectral_contrast(S=stft, sr=sample_rate).T,axis=0) #Contraste espectral da STFT
+    tonnetz = np.mean(librosa.feature.tonnetz(y=librosa.effects.harmonic(X), sr=sample_rate).T,axis=0) #Tonnetz do arquivo de som
     return mfccs,chroma,mel,contrast,tonnetz
 
+#procura todos os arquviso com a extensao .wav dentro de uma pasta e extrai as caracteristicas de cada arquivo
 def parse_audio_files(parent_dir,sub_dirs,file_ext='*.wav'):
     features, labels = np.empty((0,193)), np.empty(0)
     for label, sub_dir in enumerate(sub_dirs):
         for fn in glob.glob(os.path.join(parent_dir, sub_dir, file_ext)):
-            print("Extracting features from", fn)
+            print("Extraindo caracteristicas de ", fn)
             try:
-                mfccs, chroma, mel, contrast,tonnetz = extract_feature(fn)
-                ext_features = np.hstack([mfccs,chroma,mel,contrast,tonnetz])
-                features = np.vstack([features,ext_features])
-                labels = np.append(labels, fn.split('/')[2].split('-')[1])
+                mfccs, chroma, mel, contrast,tonnetz = extract_feature(fn) #pega as caracteristicas de um arquivo de som
+                ext_features = np.hstack([mfccs,chroma,mel,contrast,tonnetz]) #coloca todas em formato de pilha
+                features = np.vstack([features,ext_features]) #serializa
+                labels = np.append(labels, fn.split('/')[2].split('-')[1]) #cria os labels para o arquivo
             except: 
-                print("Error handling file", fn)
+                print("Erro ao processar o arquivo", fn) #caso o arquivo esteja corrompido
                 pass
     return np.array(features), np.array(labels, dtype = np.int)
 
+#
 def one_hot_encode(labels):
     n_labels = len(labels)
     n_unique_labels = len(np.unique(labels))
@@ -107,16 +114,18 @@ def one_hot_encode(labels):
     one_hot_encode[np.arange(n_labels), labels] = 1
     return one_hot_encode
 
+#pasta onde estão os arquivos a serem categorizados
 parent_dir = 'Sound-Data'
 
 #sub_dirs = ['fold1','fold2','fold3']
 
+#sub diretorios a serem pesquisados
 sub_dirs = ['fold1']
 
-print("Parsing audio files ...")
+print("Lendo arquivos de som ...")
 features, labels = parse_audio_files(parent_dir,sub_dirs)
 
-print("Encoding labels")
+print("Codificando caracteristicas ...")
 
 labels = one_hot_encode(labels)
 
@@ -126,13 +135,13 @@ train_y = labels[train_test_split]
 test_x = features[~train_test_split]
 test_y = labels[~train_test_split]
 
-print("Training the neural network ...")
+print("Treinando a rede neural ...")
 
 
 import tensorflow as tf
 from sklearn.metrics import precision_recall_fscore_support
 
-training_epochs = 5000
+training_epochs = 5000 #numero de iteracoes para o treinamento ...
 n_dim = features.shape[1]
 n_classes = 10
 n_hidden_units_one = 280 
@@ -140,6 +149,7 @@ n_hidden_units_two = 300
 sd = 1 / np.sqrt(n_dim)
 learning_rate = 0.01
 
+#inserindo valores padrao nas variaveis
 X = tf.placeholder(tf.float32,[None,n_dim])
 Y = tf.placeholder(tf.float32,[None,n_classes])
 
@@ -159,7 +169,7 @@ y_ = tf.nn.softmax(tf.matmul(h_2,W) + b)
 
 init = tf.global_variables_initializer()
 
-print("calculation the cost function ...")
+print("Calculando a funcao de custo ...")
 
 cost_function = tf.reduce_mean(-tf.reduce_sum(Y * tf.log(y_), reduction_indices=[1])) 
 optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost_function)
@@ -170,7 +180,7 @@ accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 cost_history = np.empty(shape=[1],dtype=float)
 y_true, y_pred = None, None
 
-print("Starting TS session ...")
+print("Iniciando sessao do TensorFlow ...")
 with tf.Session() as sess:
     sess.run(init)
     for epoch in range(training_epochs):            
@@ -181,14 +191,16 @@ with tf.Session() as sess:
     y_pred = sess.run(tf.argmax(y_,1),feed_dict={X: test_x})
     y_true = sess.run(tf.argmax(test_y,1))
 
-print(" Ploting ...")
+print(" Plotando a funcao de custo ...")
 fig = plt.figure(figsize=(10,8))
 plt.plot(cost_history)
-plt.ylabel("Cost")
-plt.xlabel("Iterations")
+plt.ylabel("Custo")
+plt.xlabel("Iteracoes")
 plt.axis([0,training_epochs,0,np.max(cost_history)])
 plt.show()
 
 p,r,f,s = precision_recall_fscore_support(y_true, y_pred, average='micro')
-print ("F-Score:", round(f,3))
-'''
+print ("Precisao (F-Score):", round(f,3))
+
+fim = time.time()
+print("Tempo de processamento", fim-comeco)

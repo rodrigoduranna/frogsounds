5f28492c286584dd3fdaf9a94ef72bc1df05664f
Rodrigo Silva Duran <rodrigo.duran@ifms.edu.br>
2017-06-28 10:11:16 +0300

    added more comments to clarify code

--- train.py
+++ train.py
@@ -36,7 +36,7 @@ def parse_audio_files(parent_dir,sub_dirs,file_ext='*.wav'):
                 pass
     return np.array(features), np.array(labels, dtype = np.int)
 
-#
+#One hot encode é uma técnica de preenchimento de matriz onde apenas um elemento de cada linha possui o valor 1 e o restante são 0s
 def one_hot_encode(labels):
     n_labels = len(labels)
     n_unique_labels = len(np.unique(labels))
@@ -59,6 +59,7 @@ print("Codificando caracteristicas ...")
 
 labels = one_hot_encode(labels)
 
+#Cria os conjuntos de treinamento e teste, a partir dos arquivos lidos. 70% para treinamento, 30% para teste.
 train_test_split = np.random.rand(len(features)) < 0.70
 train_x = features[train_test_split]
 train_y = labels[train_test_split]
@@ -73,32 +74,34 @@ from sklearn.metrics import precision_recall_fscore_support
 
 training_epochs = 5000 #numero de iteracoes para o treinamento ...
 n_dim = features.shape[1]
-n_classes = 10
-n_hidden_units_one = 280 
-n_hidden_units_two = 300
+n_classes = 10 #numero final de sapos a serem classificados
+n_hidden_units_one = 280 #numero de neuronios na 1a camada
+n_hidden_units_two = 300 #numero de neuronios na 2a camada
 sd = 1 / np.sqrt(n_dim)
-learning_rate = 0.01
+learning_rate = 0.01 #taxa de aprendizado da funcao de custo.
 
 #inserindo valores padrao nas variaveis
-X = tf.placeholder(tf.float32,[None,n_dim])
-Y = tf.placeholder(tf.float32,[None,n_classes])
+X = tf.placeholder(tf.float32,[None,n_dim]) # input
+Y = tf.placeholder(tf.float32,[None,n_classes]) #output
 
+#Weight , bias da cadamada 1
 W_1 = tf.Variable(tf.random_normal([n_dim,n_hidden_units_one], mean = 0, stddev=sd))
 b_1 = tf.Variable(tf.random_normal([n_hidden_units_one], mean = 0, stddev=sd))
 h_1 = tf.nn.tanh(tf.matmul(X,W_1) + b_1)
 
-
+#Weight , bias da cadamada 2
 W_2 = tf.Variable(tf.random_normal([n_hidden_units_one,n_hidden_units_two], mean = 0, stddev=sd))
 b_2 = tf.Variable(tf.random_normal([n_hidden_units_two], mean = 0, stddev=sd))
 h_2 = tf.nn.sigmoid(tf.matmul(h_1,W_2) + b_2)
 
-
+#Weight , bias da cadamada de saida
 W = tf.Variable(tf.random_normal([n_hidden_units_two,n_classes], mean = 0, stddev=sd))
 b = tf.Variable(tf.random_normal([n_classes], mean = 0, stddev=sd))
 y_ = tf.nn.softmax(tf.matmul(h_2,W) + b)
 
 init = tf.global_variables_initializer()
 
+#objeto para salvar o modelo de treinamento em disco apos finalizar o treinamento.
 saver = tf.train.Saver()
 
 print("Calculando a funcao de custo ...")
@@ -109,21 +112,26 @@ optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost_funct
 correct_prediction = tf.equal(tf.argmax(y_,1), tf.argmax(Y,1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
+#vetor com os valores calculados da função de custo
 cost_history = np.empty(shape=[1],dtype=float)
 y_true, y_pred = None, None
 
 print("Iniciando sessao do TensorFlow ...")
 with tf.Session() as sess:
     sess.run(init)
-    for epoch in range(training_epochs):            
+    for epoch in range(training_epochs): #5000 iteracoes para treinamento...           
         print("Epoch", epoch)
-        _,cost = sess.run([optimizer,cost_function],feed_dict={X:train_x,Y:train_y})
-        cost_history = np.append(cost_history,cost)
+        _,cost = sess.run([optimizer,cost_function],feed_dict={X:train_x,Y:train_y}) #calcula a função de custo de acordo com a entrada e armazenada na saida os valores calculados
+        cost_history = np.append(cost_history,cost) #guarda o valor calculado para plotar a funcao de custo depois
     
     y_pred = sess.run(tf.argmax(y_,1),feed_dict={X: test_x})
     y_true = sess.run(tf.argmax(test_y,1))
     save_path = saver.save(sess, "model/graph.ckpt")
     print("Model saved in file: %s" % save_path)
+
+
+
+fim = time.time()
 
 print(" Plotando a funcao de custo ...")
 fig = plt.figure(figsize=(10,8))
@@ -136,5 +144,5 @@ plt.show()
 p,r,f,s = precision_recall_fscore_support(y_true, y_pred, average='micro')
 print ("Precisao (F-Score):", round(f,3))
 
-fim = time.time()
+
 print("Tempo de processamento", fim-comeco)
